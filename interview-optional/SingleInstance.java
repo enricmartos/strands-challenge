2affd11f6db25f327cdc7c20cc02695e52bb9894
enricmartos <enric.martos@gmail.com>
2020-08-06 21:48:40 +0200

    Add optional exercises

--- interview-optional/SingleInstance.java
+++ interview-optional/SingleInstance.java
@@ -3,6 +3,9 @@ package com.strands.spf;
 /**
  * <p>
  * Single instance in a multi threading environment
+ * This is a thread safe singleton: Singleton property is maintained even in multithreaded environment.
+ * To make a singleton class thread-safe, getInstance() method is made synchronized so that multiple
+ * threads can’t access it simultaneously.
  * </p>
  * 
  * @author strands
@@ -10,8 +13,19 @@ package com.strands.spf;
  */
 public class SingleInstance {
 
-  public SingleInstance() {
-    // TODO Auto-generated constructor stub
+  // private instance, so that it can only be accessed by getInstance() method
+  private static SingleInstance instance;
+
+  // private constructor
+  private SingleInstance {  }
+
+
+  synchronized public static SingleInstance getInstance() {
+    // Lazy inicialization: the object is created only if it is needed to prevent resource wastage
+    if (instance = null) {
+      instance = new SingleInstance();
+    }
+    return instance;
   }
 
   /**
